6ad78ce124353bcea6817c38ababc46a3dbadb3f
onux <oanystefy1>
2019-05-29 22:01:10 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello\nOana");
+        System.out.println(24+25);
+        System.out.println(76/3);
+        System.out.println(-5 + 8 * 6);
+        System.out.println((55+9) % 9);
+        System.out.println(20 + -3*5 / 8);
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+    }
+}
